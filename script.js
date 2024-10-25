94a5b0cb9d0572ec662e97a27e683dad54cd952a
happyzahid252 <03434441031arslan@gmail.com>
2024-10-25 21:33:37 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("goto").addEventListener("click",function(){
+        window.location.href = "google.html";
+    })
+})
